a58ae38ecd7dc9cbb58ce37f42102e0d9a1198ca
Nicholas A Christensen <nickachristensen@gmail.com>
2022-02-08 20:23:06 -0500

    commit tree a50296ffad5497824bff8f7353b288a0487e0eb6

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function saturdayFun(fun="roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+}
+
+function mondayWork(work="go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair = '*') {
+    function innerFunction(adjective = '||') {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+    return innerFunction
+}
